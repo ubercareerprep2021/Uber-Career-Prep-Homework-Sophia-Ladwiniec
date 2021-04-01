77678e76cf42e4d1a0b467577bcf337670ad8c2c
Sophia Ladwiniec <slladwiniec@dons.usfca.edu>
2021-04-01 13:03:07 -0700

    adding solution for permutations

--- Assignment-1/Part2.py
+++ Assignment-1/Part2.py
@@ -1,6 +1,14 @@
-# runtime: O(N) we only pass through the N times, N being the number of elements in the array 
-# I used a set for constant lookup time 
-# space complexity: O(N) added a set to hold the differences to get the targetSum
+def isStringPermutation(s1, s2):
+    if(len(s1) != len(s2)):
+        return False
+    for character in s1: 
+        if character in s2:
+            s2 = s2.replace(character, "")
+    
+    if(len(s2) == 0):
+        return True 
+    return False 
+
 def pairsThatEqualSum(inputArray, targetSum):
     differences = set()
     pairs = []
@@ -13,17 +21,33 @@ def pairsThatEqualSum(inputArray, targetSum):
     return pairs
 
 def main():
+    print("Pairs that equal sum: ")
     pairs = pairsThatEqualSum([1, 2, 3, 4, 5], 5)
     print("Input Array: [1, 2, 3, 4, 5] Target Sum: 5")
     print("Pairs:", pairs)
+    print("\n")
 
     pairs = pairsThatEqualSum([1, 2, 3, 4, 5], 1)
     print("Input Array: [1, 2, 3, 4, 5] Target Sum: 1")
     print("Pairs:", pairs)
+    print("\n")
 
     pairs = pairsThatEqualSum([1, 2, 3, 4, 5], 7)
     print("Input Array: [1, 2, 3, 4, 5] Target Sum: 7")
     print("Pairs:", pairs)
 
+    print("\n")
+    print("Is string permutation: ")
+
+    print("s1: asdf s2: fsda")
+    print("Permutation:", isStringPermutation("asdf", "fsda"))
+    print("\n")
+
+    print("s1: asdf s2: fsa")
+    print("Permutation:", isStringPermutation("asdf", "fsa"))
+    print("\n")
+
+    print("s1: asdf s2: fsax")
+    print("Permutation:",isStringPermutation("asdf", "fsax"))
 
 main()
