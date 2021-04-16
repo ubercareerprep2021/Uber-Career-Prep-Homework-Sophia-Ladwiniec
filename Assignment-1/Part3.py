474bab6f5784b944acb1eade679147a8f45a905b
sophialadwiniec <slladwiniec@dons.usfca.edu>
2021-04-15 19:18:56 -0700

    adding queue code

--- Assignment-1/Part3.py
+++ Assignment-1/Part3.py
@@ -21,6 +21,33 @@ class Stack:
     def size(self):
         return len(self.stack)
 
+class Queue:
+    def __init__(self):
+        self.queue = []
+    
+    def enqueue(self,num):
+        self.queue.append(num)
+    
+    def dequeue(self):
+        if(self.isEmpty()):
+            return False 
+        removed = self.queue[0]
+        self.queue.remove(removed)
+        return removed
+
+    def rear(self):
+        return self.queue[-1]
+    
+    def front(self):
+        return self.queue[0]
+    
+    def size(self):
+        return len(self.queue)
+    
+    def isEmpty(self):
+        if(len(self.queue) == 0):
+            return True 
+        return False 
 def main():
     myStack = Stack()
     myStack.push(42)
@@ -31,5 +58,15 @@ def main():
     print("Size of stack:", myStack.size())
     popped_value = myStack.pop()
     print("Popped value:", popped_value)
+
+    myQueue = Queue()
+    myQueue.enqueue(1)
+    myQueue.enqueue(2)
+    myQueue.enqueue(3)
+    print("Size of queue:", myQueue.size())
+    print("Front of queue:", myQueue.front())
+    print("Rear of queue:", myQueue.rear())
+    dequeuedItem = myQueue.dequeue()
+    print('Dequeue item:', dequeuedItem)
 
 main()
